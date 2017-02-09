5549986e81dae03e8ae5ca6abcc7f8efbbc5e527
batman-1 <646563557@qq.com>
2017-02-10 00:14:00 +0800

    'resume'

---
+++ resumer/src/components/ResumePreview.vue
@@ -0,0 +1,18 @@
+<template>
+	<div id="resumePreview">
+		I am resumePreview
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'ResumePreview'//一个组件  是不知道自己的名字的除非你给他一个name
+	}
+</script>
+
+<style>
+	#resumePreview{
+		background-color: #fff;
+		box-shadow: 0 1px 3px 0 rgba(0,0,0,0.25)
+	}
+</style>
